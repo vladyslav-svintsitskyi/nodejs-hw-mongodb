20bde5baaa80d485e2afcab29cf1021dd22381e1
Vladyslav Svintsitskyi <svintsitskyi1999@gmail.com>
2025-03-02 20:33:45 +0100

    add hw

---
+++ src/models/contact.js
@@ -0,0 +1,32 @@
+import mongoose, { model } from 'mongoose';
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const ContactsCollection = model('contacts', contactSchema);
