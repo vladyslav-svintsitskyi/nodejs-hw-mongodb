900c00ec778cf87adc1735a833b4f6dc0f8ee325
Vladyslav Svintsitskyi <svintsitskyi1999@gmail.com>
2025-04-02 22:34:27 +0200

    fix patch request

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -112,7 +112,7 @@ export const patchContactController = async (req, res, next) => {
   res.json({
     status: 200,
     message: 'Successfully patched a contact!',
-    data: result.contact,
+    data: result,
   });
 };
 
